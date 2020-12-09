6d495eb7b8df5d272066cc8bd5f22e449f619b65
gjoigneaux <60109809+gjoigneaux@users.noreply.github.com>
2020-12-09 20:43:42 +0100

    Update groupomania.sql

--- groupomania.sql
+++ groupomania.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE SCHEMA `groupomania` ;
 
 USE groupomania;
@@ -25,32 +24,4 @@ CREATE TABLE `messages` (
   PRIMARY KEY (`idMESSAGES`),
   KEY `idUSERS` (`idUSERS`),
   CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`idUSERS`) REFERENCES `user` (`id`) ON DELETE CASCADE
-=======
-CREATE SCHEMA `groupomania` ;
-
-USE groupomania;
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `isAdmin` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `messages` (
-  `idMESSAGES` int(11) NOT NULL AUTO_INCREMENT,
-  `idPARENT` int(11) NOT NULL,
-  `idUSERS` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `multimedia` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`idMESSAGES`),
-  KEY `idUSERS` (`idUSERS`),
-  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`idUSERS`) REFERENCES `user` (`id`) ON DELETE CASCADE
->>>>>>> 026866f241935927ac6a8a61da87b49953559923
 ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4;
